6e0af2b1eb648afa6610f98ec687b4a34e0caef9
gulledgepo <gulledgepo@gmail.com>
2017-09-05 11:08:01 -0700

    Initial Commit

---
+++ Week3Assignment3Part1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Week3Assignment3Part1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetFiles = new System.Windows.Forms.Button();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetFiles
+            // 
+            this.btnGetFiles.Location = new System.Drawing.Point(12, 173);
+            this.btnGetFiles.Name = "btnGetFiles";
+            this.btnGetFiles.Size = new System.Drawing.Size(81, 23);
+            this.btnGetFiles.TabIndex = 0;
+            this.btnGetFiles.Text = "Get Files";
+            this.btnGetFiles.UseVisualStyleBackColor = true;
+            this.btnGetFiles.Click += new System.EventHandler(this.btnGetFiles_Click);
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.HorizontalScrollbar = true;
+            this.lstDisplay.Location = new System.Drawing.Point(12, 33);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(621, 134);
+            this.lstDisplay.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 202);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(81, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear Display";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(180, 202);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "Write to File";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(180, 173);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "Read File";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(558, 202);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 5;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGetFiles;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 239);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.btnGetFiles);
+            this.Name = "Form1";
+            this.Text = "Exercise 1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetFiles;
+        private System.Windows.Forms.ListBox lstDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ Week3Assignment3Part1/Form1.cs
@@ -0,0 +1,147 @@
+﻿//Paxston Gulledge
+//August 22, 2017, Week 3 Assignment 3 Part 1
+//Simple program to display current working directory and then list each file and its size
+//Writes to a file and reads from the file. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //allows for working with files
+
+namespace Week3Assignment3Part1
+{
+    public partial class Form1 : Form
+    {
+        private StreamWriter fil; // declare a file string object
+        private StreamReader inFile; //Declaring a file stream object
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstDisplay.Items.Clear();
+        }
+
+        private void btnGetFiles_Click(object sender, EventArgs e)
+        {
+            lstDisplay.Items.Clear();
+            //Try to read from current directory
+            try
+            {
+                //Create a new directory class at the current working directory
+                DirectoryInfo dir = new DirectoryInfo(".");
+                //Update our label to display the information and current directory, and a line break
+                lstDisplay.Items.Add("The current directory is " + Directory.GetCurrentDirectory());
+                lstDisplay.Items.Add("The files contained inside are:");
+                lstDisplay.Items.Add("");
+                //For each file in the current directory we are going to add the file name, its size and a line break
+                foreach (FileInfo fil in dir.GetFiles("."))
+                {
+                    lstDisplay.Items.Add(fil.Name);
+                    lstDisplay.Items.Add("Size: " + fil.Length.ToString("N0") + "KB");
+                }
+            }
+            //If we couldn't we let them the user know it failed.
+            catch
+            {
+                lstDisplay.Items.Clear();
+                lstDisplay.Items.Add("The process failed.");
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //String to use as our inValue for current line being read
+            string inValue;
+            //clear our display
+            lstDisplay.Items.Clear();
+            //If the file is there we will proceed, if not we will let the user know there is no file
+            if (File.Exists("fileinfo.txt"))
+            {
+                //Try catch to make sure we can read the file, if something goes wrong we display an error message.
+                try
+                {
+                    inFile = new StreamReader("fileinfo.txt");
+                    //While our string for current line is not null, we will do our actions, null being end of file
+                    while ((inValue = inFile.ReadLine()) != null)
+                    {
+                        //Each line is added to our list box
+                        lstDisplay.Items.Add(inValue);
+                    }
+                    inFile.Close();
+                }
+                catch (System.IO.IOException exc)
+                {
+                    lstDisplay.Items.Clear();
+                    lstDisplay.Items.Add(exc.Message);
+                }
+            }
+            else
+            {
+                lstDisplay.Items.Clear();
+                lstDisplay.Items.Add("File unavailable");
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fil = new StreamWriter("fileinfo.txt");
+                //For each item in the list box we are going to write it to a new line, that way when we read we can just read by line
+                foreach (var listBoxItem in lstDisplay.Items)
+                {
+                    fil.WriteLine(listBoxItem);
+                }
+                MessageBox.Show("File info written to file fileinfo.txt");
+                fil.Close();
+            }
+            catch (DirectoryNotFoundException exc)
+            {
+                lstDisplay.Items.Clear();
+                lstDisplay.Items.Add("Invalid directory" + exc.Message);
+            }
+
+            catch (System.IO.IOException exc)
+            {
+                lstDisplay.Items.Clear();
+                lstDisplay.Items.Add(exc.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Close the files when we close the form, placed in a try so no error is produced
+            //incase no files were messed with 
+            try
+            {
+                fil.Close();
+                inFile.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            //Populate listbox with help information
+            lstDisplay.Items.Clear();
+            lstDisplay.Items.Add("Click Get Files to list the current directory and all files within the directory.");
+            lstDisplay.Items.Add("Click Write to File to save the information in the listbox to a file (even this help information!).");
+            lstDisplay.Items.Add("Click Read File to display the information located within the text file.");
+            lstDisplay.Items.Add("Click click Clear Display to clear all information in this box.");
+        }
+    }
+}
